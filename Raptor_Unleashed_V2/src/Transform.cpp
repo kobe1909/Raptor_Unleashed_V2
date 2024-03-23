59e96e5b72880921e2448d3e1d1f7ff14c803046
Kobe Van der Stappen <kobe.vdstappen@gmail.com>
2024-03-23 20:34:48 +0100

    Added transform

---
+++ Raptor_Unleashed_V2/src/Transform.cpp
@@ -0,0 +1,29 @@
+#include "Transform.h"
+
+Transform::Transform() : position(), rotation(), scale() {}
+
+Transform::Transform(glm::vec3 n_position, glm::vec3 n_rotation, glm::vec3 n_scale)
+	: position(n_position), rotation(n_rotation), scale(n_scale) {}
+
+glm::mat4 Transform::GetModelMatrix() {
+	glm::mat4 matrix = glm::mat4(1.0f);
+
+	// Translate the matrix
+	matrix = glm::translate(matrix, position);
+
+	// Rotate the matrix
+	// x
+	matrix = glm::rotate(matrix, glm::radians(rotation.x), glm::vec3(1, 0, 0));
+
+	// y
+	matrix = glm::rotate(matrix, glm::radians(rotation.y), glm::vec3(0, 1, 1));
+
+	// z 
+	matrix = glm::rotate(matrix, glm::radians(rotation.z), glm::vec3(0, 0, 1));
+
+	// Apply Scale
+	matrix = glm::scale(matrix, scale);
+
+	// Return matrix
+	return matrix;
+}
---
+++ Raptor_Unleashed_V2/src/Transform.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+class Transform {
+	glm::vec3 position;
+	glm::vec3 rotation;
+	glm::vec3 scale;
+
+	Transform();
+	Transform(glm::vec3, glm::vec3, glm::vec3);
+
+	glm::mat4 GetModelMatrix();
+};
+
